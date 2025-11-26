6943cf594e5f2b8889ab1092698fb6848dabc9ba
Mohd Saif <iamg6102@gmail.com>
2025-11-26 14:21:50 +0530

    Chnage isAuthenticated for the mobile

--- Spend Mate/spendMateBackend/src/middleware/auth.js
+++ Spend Mate/spendMateBackend/src/middleware/auth.js
@@ -3,19 +3,29 @@ import { User } from "../models/user.model.js"
 import apiError from "../utils/apiError.js"
 
 export const isAuthenticated = async (req, res, next) => {
-    const token = req.cookies.spendmate_token
+  const authHeader = req.headers.authorization
 
-    if (!token) {
-        throw new apiError(401, "Unauthorized")
-    }
+  if (!authHeader) {
+    throw new apiError(401, "No authorization header provided")
+  }
 
+  const [type, token] = authHeader.split(" ")
+
+  if (type.toLowerCase() !== "bearer" || !token) {
+    throw new apiError(401, "Invalid authorization format")
+  }
+
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
     req.user = await User.findById(decoded.id).select("-password")
 
     if (!req.user) {
-        throw new apiError(404, "User not found")
+      throw new apiError(404, "User not found")
     }
 
     next()
+  } catch (err) {
+    throw new apiError(401, "Invalid or expired token")
+  }
 }
