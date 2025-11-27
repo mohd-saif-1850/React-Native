8d229090a9af5a557162fdf5e716e520741c6b89
Mohd Saif <iamg6102@gmail.com>
2025-11-27 22:15:43 +0530

    changes the feedback model and remove all the authentication

--- Spend Mate/spendMateBackend/src/models/feedback.model.js
+++ Spend Mate/spendMateBackend/src/models/feedback.model.js
@@ -1,18 +1,21 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const feedbackSchema = new Schema({
-    userId: {
-        type: String,
-        required: true
+const feedbackSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     message: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     }
-})
+  },
+  { timestamps: true }
+);
 
 export const Feedback = mongoose.models.Feedback || mongoose.model("Feedback", feedbackSchema);
