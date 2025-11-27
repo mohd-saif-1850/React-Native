c6124c52d42000d829d549fb7e2acc5d797596af
Mohd Saif <iamg6102@gmail.com>
2025-11-27 12:52:15 +0530

    authentication done in the refine title route

--- Spend Mate/spendMateBackend/src/routes/refineTitle.route.js
+++ Spend Mate/spendMateBackend/src/routes/refineTitle.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { refineTitle } from "../controllers/refineTitle.controller.js";
+import { isAuthenticated } from "../middleware/auth.js"
 
 const router = express.Router();
 
-router.route("/refine-title").post(refineTitle);
+router.route("/refine-title").post(isAuthenticated,refineTitle);
 
 export default router;
