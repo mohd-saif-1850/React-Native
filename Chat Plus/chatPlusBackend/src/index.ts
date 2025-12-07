634a520c4b9058daf231e1d002df47304617a98c
Mohd Saif <iamg6102@gmail.com>
2025-12-07 10:21:53 +0530

    web scoket configuration done maybe

--- Chat Plus/chatPlusBackend/src/index.ts
+++ Chat Plus/chatPlusBackend/src/index.ts
@@ -3,9 +3,22 @@ dotenv.config()
 
 import app from "./app"
 import { dbConnect } from "./database/dbConnect"
+import { registerSocketServer } from "./webSocket";
+import http from "http"
 
 
 dbConnect();
-app.listen(process.env.PORT, () => {
-    console.log("Your App is Running on the Port : ",process.env.PORT)
+// app.listen(process.env.PORT, () => {
+//     console.log("Your App is Running on the Port : ",process.env.PORT)
+// })
+
+//Web Socket Configuration Here !
+const server = http.createServer(app)
+
+registerSocketServer(server)
+
+const PORT = process.env.PORT
+
+server.listen(PORT, () => {
+    console.log(`Web Socket Server is Listening on Port : ${PORT}`)
 })
