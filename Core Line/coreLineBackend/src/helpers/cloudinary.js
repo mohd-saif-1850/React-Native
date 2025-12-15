d1a103899baa0f350f5ed4a1f807c261245ce981
Mohd Saif <iamg6102@gmail.com>
2025-12-15 22:28:34 +0530

    just trying

--- Core Line/coreLineBackend/src/helpers/cloudinary.js
+++ Core Line/coreLineBackend/src/helpers/cloudinary.js
@@ -1,13 +1,4 @@
 import { v2 as cloudinary } from "cloudinary"
 
-if (!process.env.CLOUDINARY_API_KEY) {
-  throw new Error("Missing Cloudinary API key")
-}
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-})
 
 export default cloudinary
--- Core Line/coreLineBackend/src/utils/uploadToCloudinary.js
+++ Core Line/coreLineBackend/src/utils/uploadToCloudinary.js
@@ -1,10 +1,36 @@
 import cloudinary from "../helpers/cloudinary.js"
 import fs from "fs"
 
+let isConfigured = false
+
+const configureCloudinary = () => {
+  if (isConfigured) return
+
+  const {
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
+  } = process.env
+
+  if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
+    throw new Error("Cloudinary env variables are missing")
+  }
+
+  cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+  })
+
+  isConfigured = true
+}
+
 const uploadToCloudinary = async (localFilePath, folder = "coreline") => {
+  configureCloudinary()
+
   const result = await cloudinary.uploader.upload(localFilePath, {
     folder,
-    resource_type: "auto"
+    resource_type: "auto",
   })
 
   fs.unlinkSync(localFilePath)
