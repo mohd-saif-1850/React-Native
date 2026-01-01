5735d9e621ed93f9afedd98147344f5b8de406fe
Mohd Saif <iamg6102@gmail.com>
2026-01-01 12:54:16 +0530

    mobile allowed

--- Mumentum/mumentumbackend/src/app.ts
+++ Mumentum/mumentumbackend/src/app.ts
@@ -9,7 +9,8 @@ app.use(express.json())
 app.use(helmet())
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*"
+  origin: process.env.FRONTEND_URL || "*",
+  credentials: true
 }))
 
 app.get("/",(req,res) => {
