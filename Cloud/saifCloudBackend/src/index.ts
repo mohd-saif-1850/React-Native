f312c074101ac26bfddef68bdf759a1930acd41d
Mohd Saif <iamg6102@gmail.com>
2026-02-07 14:39:30 +0530

    Added the required packages and types

--- Cloud/saifCloudBackend/src/index.ts
+++ Cloud/saifCloudBackend/src/index.ts
@@ -1,19 +1,5 @@
-import sendEmail from "saifstack-email";
+import app from "./express";
 
-async function start() {
-  try {
-    await sendEmail({
-      api: "1b408cfa24a2cbea",
-      domainName: "SaifCloud",
-      email: "mohdsaif18500@gmail.com",
-      subject: "Welcome",
-      otp: "123456"
-    });
-
-    console.log("Email sent successfully!");
-  } catch (err) {
-    console.error("Error sending email:", err);
-  }
-}
-
-start();
+app.listen(3000, () => {
+  console.log("Saif Cloud Server is running at Port 3000")
+})
